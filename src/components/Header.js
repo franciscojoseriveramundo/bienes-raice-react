47fc3087d9b7d38fae2f7e3ca2e7f4e89de2687c
Francisco <francisco.rivera@servitron.net>
2022-05-08 05:24:13 -0500

    Se realiza la edición del menú de la aplicación (icono hamburger)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link} from 'react-router-dom';
 import logo from '../images/bienes_raices.png';
+import hamburgermenu from '../images/menu.svg';
 
 const Header = () =>{
     return(
@@ -13,8 +14,8 @@ const Header = () =>{
                         </Link>
 
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-site" aria-controls="navbar-site" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                            <img src={hamburgermenu} width="24"></img>
+                        </button>
 
                         <div className="collapse navbar-collapse" id="navbar-site">
                             <ul className="navbar-nav mx-auto mb-2 mb-lg-0 nav-bar-site">
