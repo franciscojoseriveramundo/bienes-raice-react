c5555813993650b3ca21b4fe3707aa7fed1090a5
Francisco <francisco.rivera@servitron.net>
2022-05-11 13:04:08 -0500

    Diseño del selectname "Sexo" del formulario de registro.

--- src/components/Register.js
+++ src/components/Register.js
@@ -17,23 +17,30 @@ const Register = () =>{
                     </div>
                 </div>
 
-                <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 col-user-data">
+                <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 col-user-data">
                     <label className='lbl-col-user-data'>* Nombre(s)</label>
                     <input type="text" className="form-control" id="txtName" placeholder="Ingresa tu nombre(s)"/>
                 </div>
-                <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 col-user-data">
+                <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 col-user-data">
                     <label className='lbl-col-user-data'>* Apellido(s)</label>
                     <input type="text" className="form-control" id="txtLastName" placeholder="Ingresa tu(s) apellidos(s)"/>
                 </div>
-                <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 col-user-data">
+                <div className='col-sm-12 col-md-6 col-lg-6 col-xl-6 col-user-data'>
+                    <label className='lbl-col-user-data'>* Sexo:</label>
+                    <select name="Sexo" className='form-control' id="cbxSexo">
+                        <option value="Hombre">Hombre</option>
+                        <option value="Mujer">Mujer</option>
+                    </select>
+                </div>
+                <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 col-user-data">
                     <label className='lbl-col-user-data'>* Número de teléfono</label>
                     <input type="text" className="form-control" id="txtPhone" placeholder="Ingresa tu número de teléfono"/>
                 </div>
-                <div class="col-sm-12 col-md-6 col-lg-6 col-xl-6 col-user-data">
+                <div className="col-sm-12 col-md-6 col-lg-6 col-xl-6 col-user-data">
                     <label className='lbl-col-user-data'>* Correo electrónico</label>
                     <input type="text" className="form-control" id="txtEmail" placeholder="Ingresa tu correo electrónico"/>
                 </div>
-                <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-user-data">
+                <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-user-data">
                     <button type="submit" className="btn btn-primary btn-submit-register">Registrar</button>
                 </div>
             </div>
